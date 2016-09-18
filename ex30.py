10a6092f6dc5c4b4e05c1cdf7841439679a5f604
Katie <Katiejen81@gmail.com>
2016-09-17 22:10:22 -0500

    Work on 9-17

---
+++ ex30.py
@@ -0,0 +1,26 @@
+#Set the variable values
+people = 30
+cars = 40
+trucks = 15
+
+#Cars decision point
+if cars > people:
+    print "We should take the cars."
+elif cars < people:
+    print "We should not take the cars."
+else:
+    print "We can't decide."
+
+#Trucks decision point
+if trucks > cars:
+    print "That's too many trucks."
+elif trucks < cars:
+    print "Maybe we could take the trucks."
+else:
+    print "We still can't decide."
+
+#People and trucks decision point
+if people > trucks:
+    print "Alright, let's just take the trucks."
+else:
+    print "Fine, let's stay home then."
